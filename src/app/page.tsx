b4620671f0a862546e5f9070a42d1377cc8562c7
Sayantan <sayantan0504@gmail.com>
2023-09-21 10:33:00 +0530

    Styling Home Page and Clerk Setup🔐

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,46 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
 
-export default function Home() {
-  return <div className="font-bold">Hello World</div>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-tr from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="text-5xl font-semibold">
+              Chat with any PDF
+            </h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+
+          <p className="mt-1 max-w-xl text-lg text-slate-600">
+            Join other students, engineers, developers and
+            professionals to answer all your questions with
+            AI.
+          </p>
+
+          <div className="w-full mt-3">
+            {isAuth ? (
+              <div>upload</div>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login <LogIn className="w-5 h-5 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
